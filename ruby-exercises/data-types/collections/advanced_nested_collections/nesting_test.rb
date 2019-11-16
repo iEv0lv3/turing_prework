b21a4ced6687de078a60b9c0316a70b16174a25b
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-15 23:36:10 -0700

    Solve nesting test answers

--- ruby-exercises/data-types/collections/advanced_nested_collections/nesting_test.rb
+++ ruby-exercises/data-types/collections/advanced_nested_collections/nesting_test.rb
@@ -1,6 +1,7 @@
 require './nesting'
 require 'minitest/autorun'
 require 'minitest/pride'
+require 'pry'
 
 # The intent of this exercise is to practice working with nested collections.
 # Some tests will be able to pass without any enumeration, and others will require
@@ -16,67 +17,62 @@ require 'minitest/pride'
 class NestedTest < MiniTest::Test
 
   def test_list_of_olive_garden_employess
-    skip
-    #^^^ Un-Skip each test
-    #=======================
-    # EXAMPLE
     employees = stores[:olive_garden][:employees]
     #=======================
     assert_equal ["Jeff", "Zach", "Samantha"], employees
   end
 
   def test_pancake_ingredients
-    skip
     #=======================
-    # pancake_ingredients = <your code here>
+    pancake_ingredients = stores[:dennys][:dishes][0][:ingredients]
     #=======================
     assert_equal ["Flour", "Eggs", "Milk", "Syrup"], pancake_ingredients
   end
 
   def test_rissotto_price
-    skip
     #=======================
-    # risotto_price = <your code here>
+    risotto_price = stores[:olive_garden][:dishes][0][:price]
     #=======================
     assert_equal 12, risotto_price
   end
 
   def test_big_mac_ingredients
-    skip
     #=======================
-    # big_mac_ingredients = <your code here>
+    big_mac_ingredients = stores[:macdonalds][:dishes][0][:ingredients]
     #=======================
     assert_equal ['Bun','Hamburger','Ketchup','pickles'], big_mac_ingredients
   end
 
   def test_list_of_restaurants
-    skip
     #=======================
-    # store_names = <your code here>
+    store_names = stores.keys
     #=======================
     assert_equal [:olive_garden, :dennys, :macdonalds], store_names
   end
 
   def test_list_of_dishes_names_for_olive_garden
-    skip
     #=======================
-    # dishes_names = <your code here>
+    dishes_names = stores[:olive_garden][:dishes].map { |dish| dish[:name] }
     #=======================
     assert_equal ['Risotto', 'Steak'], dishes_names
   end
 
   def test_list_of_employees_across_all_restaurants
-    skip
     #=======================
-    # employee_names = <your code here>
+    employee_names = []
+    stores.each do |k,v|
+      employee_names.concat(v[:employees])
+    end
     #=======================
     assert_equal ["Jeff","Zach","Samantha","Bob","Sue","James","Alvin","Simon","Theodore"], employee_names
   end
 
   def test_list_of_all_ingredients_across_all_restaurants
-    skip
     #=======================
-    # ingredients = <your code here>
+    ingredients = []
+    stores.each do |k,v|
+      ingredients.concat(v[:dishes].map { |dish| dish[:ingredients] }).flatten!
+    end
     #=======================
     assert_equal ["Rice",
                   "Cheese",
@@ -99,9 +95,8 @@ class NestedTest < MiniTest::Test
   end
 
   def test_full_menu_price_for_olive_garden
-    skip
     #=======================
-    # full_menu_price = <your code here>
+
     #=======================
     assert_equal 27, full_menu_price
   end

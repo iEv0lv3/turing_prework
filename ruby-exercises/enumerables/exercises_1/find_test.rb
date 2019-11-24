99f2fd5d7ed570abc791555ae9d32aefc2b0863d
Jordan Williams <jordan.wallace.williams@gmail.com>
2019-11-24 15:28:12 -0700

    Solve find tests

--- ruby-exercises/enumerables/exercises_1/find_test.rb
+++ ruby-exercises/enumerables/exercises_1/find_test.rb
@@ -34,52 +34,65 @@ class FindTest < Minitest::Test
   end
 
   def test_cannot_find_3_letter_words
-    skip
     words = ["piglet", "porridge", "bear", "blueberry"]
-    # Your code goes here
-    assert_equal nil, found
+    found = words.find do |word|
+      if word.length == 3
+        word
+      end
+    end
+    assert_nil nil, found
   end
 
   def test_find_13
-    skip
     numbers = [2, 13, 19, 8, 3, 27]
-    # Your code goes here
+    found = numbers.find do |num|
+      num if num == 13
+    end
     assert_equal 13, found
   end
 
   def test_find_first_even_number
-    skip
     numbers = [3, 7, 13, 11, 10, 2, 17]
-    # Your code goes here
+    found = numbers.find do |num|
+      num if num.even?
+    end
     assert_equal 10, found
   end
 
   def test_find_first_multiple_of_3
-    skip
     numbers = [2, 8, 9, 27, 24, 5]
-    # Your code goes here
+    found = numbers.find do |num|
+      num if (num % 3).zero?
+    end
+
     assert_equal 9, found
   end
 
   def test_find_first_word_starting_with_q
-    skip
     words = ["weirdo", "quill", "fast", "quaint", "quitter", "koala"]
-    # Your code goes here
+
+    found = words.find do |word|
+      word if word[0] == 'q'
+    end
+
     assert_equal "quill", found
   end
 
   def test_find_first_word_ending_with_er
-    skip
     words = ["biggest", "pour", "blight", "finger", "pie", "border"]
-    # Your code goes here
+    found = words.find do |word|
+      word if word.include?('er')
+    end
+
     assert_equal "finger", found
   end
 
   def test_find_first_number_greater_than_20
-    skip
     numbers = [1, 8, 19, 21, 29, 31, 34]
-    # Your code goes here
+    found = numbers.find do |num|
+      num if num > 20
+    end
+
     assert_equal 21, found
   end
-
 end
